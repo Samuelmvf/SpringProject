d0abbe71f557d8a395e9fb23ad74113450745893
Samuel Maia <samuelmvf@gmail.com>
2020-05-05 13:34:08 -0300

    Implementando camada Entity

---
+++ src/main/java/com/treinamento/SpringProject/entity/ClienteEntity.java
@@ -0,0 +1,40 @@
+package com.treinamento.SpringProject.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Entity
+@Table(name = "tbl_cliente")
+@SuppressWarnings("serial")
+public class ClienteEntity implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy =  GenerationType.SEQUENCE, generator = "cliente_sequence")
+	@SequenceGenerator(name = "cliente_sequence", sequenceName = "cliente_id_sequence", allocationSize = 1)
+	@Column(name = "id_cliente", nullable = false)
+	private Long id;
+	
+	@Column(name = "nome", nullable = false)
+	private String nome;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+}
---
+++ src/main/java/com/treinamento/SpringProject/entity/VeiculoEntity.java
@@ -0,0 +1,43 @@
+package com.treinamento.SpringProject.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Entity
+@Table(name = "tbl_veiculo")
+@SuppressWarnings("serial")
+public class VeiculoEntity implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "veiculo_sequence")
+	@SequenceGenerator(name = "veiculo_sequence", sequenceName = "veiculo_id_sequence", allocationSize = 1)
+	@Column(name = "id_veiculo", nullable = false)
+	private Long id;
+	
+	@Column(name = "montadora", nullable = false)
+	private String montadora;
+	
+	@Column(name = "modelo", nullable = false)
+	private String modelo;
+	
+	@Column(name = "versao", nullable = false)
+	private Double versao;
+	
+}
